6a434cb85afa4287dfb5d7e4091eef19b184468c
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-01-02 08:47:45 -0700

    trained weights + biases

--- handwriting/train.js
+++ handwriting/train.js
@@ -1,35 +1,83 @@
+// https://github.com/junku901/machine_learning
+
 const ml = require('machine_learning');
-var x = [[1,1,1,0,0,0],
-  [1,0,1,0,0,0],
-  [1,1,1,0,0,0],
-  [0,0,1,1,1,0],
-  [0,0,1,1,0,0],
-  [0,0,1,1,1,0]];
-var y = [[1, 0],
-  [1, 0],
-  [1, 0],
-  [0, 1],
-  [0, 1],
-  [0, 1]];
+const fs = require('fs');
+const MAX_SAMPLES = Number(process.env.MAX_SAMPLES) || 10;
+const MAX_TESTS = Number(process.env.MAX_TESTS) || 10;
 
-var classifier = new ml.LogisticRegression({
-  'input' : x,
-  'label' : y,
-  'n_in' : 6,
-  'n_out' : 2
+function makeLabel(val) {
+  const label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  label[val] = 1;
+  return label;
+}
+
+function predToInt(vals) {
+  let max = 0;
+  let maxIndex = 0;
+  vals.forEach((val, index) => {
+    if (val > max) {
+      max = val;
+      maxIndex = index;
+    }
+  });
+  return maxIndex;
+}
+
+fs.readFile('./data/train.csv', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const file = data.toString().split('\n');
+  const x = [];
+  const y = [];
+  const testX = [];
+  const testY = [];
+
+  file.forEach((row, i) => {
+    if (i > 0 && i < MAX_SAMPLES + MAX_TESTS + 1) {
+      const data = row.split(',').map(val => Number(val));
+      const label = data.splice(0, 1).map(val => Number(val));
+
+      if (i < MAX_SAMPLES + 1) {
+        x.push(data);
+        y.push(makeLabel(label[0]));
+      } else {
+        testX.push(data);
+        testY.push(label);
+      }
+    }
+  });
+
+  const classifier = new ml.LogisticRegression({
+    'input': x,
+    'label': y,
+    'n_in': x[0].length,
+    'n_out': y[0].length,
+  });
+
+  classifier.set('log level', 1);
+  classifier.train({
+    'lr': 0.01,
+    'epochs': 800,
+  });
+
+  console.log(JSON.stringify({ W: classifier.W, b: classifier.b }));
+
+  let correct = 0;
+  let wrong = 0;
+  testX.forEach((test, i) => {
+    const prediction = classifier.predict([test]);
+    const digit = predToInt(prediction[0]);
+    console.log(`Input: ${testY[i]}`);
+    // console.log(`Prediction: ${prediction}`);
+    console.log(`Result : ${digit}`);
+    if (digit == testY[i]) {
+      correct += 1;
+    } else {
+      wrong += 1;
+    }
+  });
+
+  console.log(`Success: ${parseInt(correct / testX.length * 100, 10)}%  (${correct}/${wrong})`);
 });
-
-classifier.set('log level',1);
-
-var training_epochs = 800, lr = 0.01;
-
-classifier.train({
-  'lr' : lr,
-  'epochs' : training_epochs
-});
-
-x = [[1, 1, 0, 0, 0, 0],
-  [0, 0, 0, 1, 1, 0],
-  [1, 1, 1, 1, 1, 0]];
-
-console.log("Result : ",classifier.predict(x));
