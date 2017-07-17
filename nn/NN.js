299abb44df65a94f1ec3bc6dfbcbc06dd37fb36c
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-07-17 06:49:28 -0600

    Stub out cost function + regularization term calculation methods

--- nn/NN.js
+++ nn/NN.js
@@ -24,6 +24,7 @@ function NN(layersConfig, settings = {}) {
   });
 
   this.layers.shift();
+  this.regularization = settings.regularization || 0.001;
 
   this.hiddenActivator = settings.hiddenActivator || 'ReLU';
   this.outputActivator = settings.outputActivator || 'Softmax';
@@ -51,23 +52,47 @@ NN.prototype._forward = function (inputs, l = 0) {
 };
 
 /**
+ * J() => (-1/m) * sum(1:m, sum(1:K, (y[k, i] * log(h(x[i]))[k] + (1 - y[k, i]) * log(1 - h(x[i])[k])) + _regularize()
+ *
+ * m = number of training examples
+ * L = total number of layers in the network
+ * K = number of output units/classes
+ * s[l] = number of units (not counting bias unit) in layer l
+ * y[k, i] = kth unit of the expected output vector given the ith training sample
+ * h(x[i]) = output vector from network during forward phase, given ith training sample
+ * h(x[i])[k] = kth unit of the actual output vector given the ith training sample
  *
  * @param {Array<number>} expected Labels vector at a given input x
  * @param {Array<number>} actual Activations vector from the entire network form a given input x
+ * @param {number} totalTrainingExamples
  * @private
  */
-NN.prototype._cost = function(expected, actual) {
+NN.prototype._cost = function(expected, actual, totalTrainingExamples) {
   if (expected.length !== actual.length) {
     throw new Error('Input size mismatch during cost calculation');
   }
 
-  // SUM(||expected - actual||^2)
   const sum = expected.reduce((acc, el, i) => {
     const diff = el - actual[i];
     return acc + diff * diff;
   }, 0);
 
-  return 0.5 * sum;
+  const coeff = - (1 / totalTrainingExamples);
+  return coeff * sum;
+};
+
+/**
+ * reg() => (lambda/2 * m) * sum(1:L-1, sum(1:s[l], sum(1:s[l] + 1, W[j, i, l]^2)))
+ *
+ * m = number to training examples
+ *
+ * @param {number} totalTrainingExamples
+ * @private
+ * @return {number}
+ */
+NN.prototype._regularize = function (totalTrainingExamples) {
+  const coeff = this.regularization / (2 * totalTrainingExamples);
+  throw new Error('TODO: implement regularization term calculation');
 };
 
 export default NN;
--- nn/NN.spec.js
+++ nn/NN.spec.js
@@ -3,10 +3,54 @@ import { expect } from 'chai';
 
 describe('NN', () => {
   it('Network has right amount of layers', () => {
-    const net = new NN([1, 2, 3]);
+    //       ()
+    //       / \
+    //     ()   ()    Layer 0
+    //    / \`.`/ \
+    //   /_,'\ /`._\
+    //  ()   ()    () Layer 1
+    let net = new NN([1, 2, 3]);
     expect(net.layers.length).to.equal(2);
     expect(net.layers[0].length).to.equal(2);
     expect(net.layers[1].length).to.equal(3);
+
+    //     ()   ()
+    //     | \ / |
+    //     |.` `.|
+    //     ()   ()  Layer 0
+    //     | \ / |
+    //     |.` `.|
+    //     ()   ()  Layer 1
+    net = new NN([2, 2, 2]);
+    expect(net.layers.length).to.equal(2);
+    expect(net.layers[0].length).to.equal(2);
+    expect(net.layers[1].length).to.equal(2);
+
+    //     ()   ()
+    //   / | \ / | \
+    // ()  ()   ()  ()  Layer 0
+    // | \/| \ /| \/|
+    // ()  ()   ()  ()  Layer 1
+    //   \ | \ /|  /
+    //     ()   ()      Layer 2
+    net = new NN([2, 4, 4, 2]);
+    expect(net.layers.length).to.equal(3);
+    expect(net.layers[0].length).to.equal(4);
+    expect(net.layers[1].length).to.equal(4);
+    expect(net.layers[2].length).to.equal(2);
+
+    //     ()   ()
+    //   / | \ / | \
+    // ()  ()   ()  ()  Layer 0
+    // | \/| \ /| \/|
+    // ()  ()   ()  ()  Layer 1
+    //   `._\  / _,'
+    //       ()         Layer 2
+    net = new NN([2, 4, 4, 1]);
+    expect(net.layers.length).to.equal(3);
+    expect(net.layers[0].length).to.equal(4);
+    expect(net.layers[1].length).to.equal(4);
+    expect(net.layers[2].length).to.equal(1);
   });
 
   it('Layers have right amount of weights (including T0 (bias)', () => {
@@ -40,5 +84,55 @@ describe('NN', () => {
 
     expect(output.length).to.equal(1);
     expect(output[0]).to.equal(a1);
+  });
+
+  it('Computes regularization term', () => {
+    const net = new NN([1, 1, 1], { regularization: 1.0 });
+    const W = [
+      [1, 2],
+      [3, 4],
+    ];
+    //      Input -----------------------+
+    // Bias --------------------+        |
+    //                          |        |
+    //                          v        v
+    net.layers[0][0].weights = [W[0][0], W[0][1]];
+    net.layers[1][0].weights = [W[1][0], W[1][1]];
+
+    const regu = net._regularize(2);
+
+    //  ()  input layer
+    //  ()  => W := [w[0, 0], w[0, 1]]
+    //  ()  => W := [w[1, 0], w[1, 1]]
+    //                    ^
+    //                     `- corresponds to bias term
+
+    const lambda = 2;
+    const totalTrainingExamples = 2;
+
+    const expectedRegVal = lambda / (2 * totalTrainingExamples) * (
+      (W[0][1] * W[0][1]) +
+      (W[1][1] * W[1][1])
+    );
+
+    expect(regu).to.equal(expectedRegVal);
+  });
+
+  it('Computes cost J(t)', () => {
+    const net = new NN([2, 4, 1], { hiddenActivator: 'ReLU', outputActivator: 'ReLU' });
+
+    // 2x2 grid
+    //
+    // 1 | 1 | 1
+    // --+---+--
+    // 1 | 1 | 0
+    // --+---+--
+    // 1 | 0 | 0
+    const inputs = [[0, 0], [2, 2]];
+    const labels = [[1], [0]];
+
+    const cost_x0 = net._cost(labels[0], net._forward(inputs[0]), 1);
+    expect(cost_x0).that.is.a('number');
+    // expect(output[0]).to.equal(a1);
   });
 });
